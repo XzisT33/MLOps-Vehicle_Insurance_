70d900cdea3ef1fc476f0e106c530485cc180725
XzcutE33 <aryanp12042002@gmail.com>
2025-01-07 15:12:09 +0530

    project structure ready, venv created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aryan Patel",
+    author_email="aryanp12042002@gmail.com",
+    packages=find_packages()
+)
